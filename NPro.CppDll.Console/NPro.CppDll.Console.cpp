7ca24bdfb19db1630ef1645d2c8c20568282ac60
zakizaki33 <zakizaki33@hotmail.com>
2022-12-09 17:01:21 +0900

    BSTRの修正

--- NPro.CppDll.Console/NPro.CppDll.Console.cpp
+++ NPro.CppDll.Console/NPro.CppDll.Console.cpp
@@ -22,6 +22,20 @@ int main()
     std::cout << GetString() << std::endl;
     std::cout << GetName() << std::endl; //文字化けする
 
+    // http://7ujm.net/C++/BSTR.html
+    char buf2[64] = "";
+    WideCharToMultiByte(
+        CP_ACP,			// コードページ ANSI コードページ
+        0,			// 処理速度とマッピング方法を決定するフラグ
+        (OLECHAR*)GetName(),//bstr,		// ワイド文字列のアドレス
+        -1,			// ワイド文字列の文字数
+        buf2,			// 新しい文字列を受け取るバッファのアドレス
+        sizeof(buf2) - 1,		// 新しい文字列を受け取るバッファのサイズ
+        NULL,			// マップできない文字の既定値のアドレス
+        NULL			// 既定の文字を使ったときにセットするフラグのアドレス
+    );
+    std::cout << buf2 << std::endl; // 成功
+
     getchar();
     return 0;
 }
